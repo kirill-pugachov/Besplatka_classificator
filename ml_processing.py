6d5695d2c515dfdc8a65e59bdeba5223c4c2e7c6
kirill-pugachov <pugachovk@gmail.com>
2017-09-19 16:33:28 +0300

    Поменял цикл оценки коэф-тов в tfidf

--- ml_processing.py
+++ ml_processing.py
@@ -13,19 +13,28 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 #from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.linear_model import SGDClassifier
 
-#читаем файл csv и разделяем метки класса и тексты для обучения
+
+def clean_text(X_train_text):
+    X_train_text = X_train_text.map(lambda kkk: kkk.lower())
+    X_train_text = X_train_text.replace('[^а-яА-Яa-zA-Z0-9]', ' ', regex=True)
+    X_train_text = X_train_text.map(lambda x: ' '.join([item.strip() for item in x.split(' ') if item not in stop_w]))
+    return X_train_text
+
+#читаем файл csv и разделяем метки класса и тексты для обучения для обучения
+#чистим тексты для обучения - все приводим к написанию строчными буквами
+#удаляем из текстов все кроме букв и цифр
 stop_w = stopwords.words('russian')
 data_train = pandas.read_csv('storage/data_base_semantica.csv',  header=None)
 y_train = data_train[0]
-X_train_text = data_train[1]
+X_train_text = clean_text(data_train[1])
 
 
+#читаем файл csv с имеющимися запросами для тестирования
 #чистим тексты для обучения - все приводим к написанию строчными буквами
 #удаляем из текстов все кроме букв и цифр
-X_train_text = X_train_text.map(lambda kkk: kkk.lower())
-X_train_text = X_train_text.replace('[^а-яА-Яa-zA-Z0-9]', ' ', regex=True)
-X_train_text = X_train_text.map(lambda x: ' '.join([item.strip() for item in x.split(' ') if item not in stop_w]))
-
+data_test = pandas.read_csv('storage/data_base_query.csv',  header=None)
+X_test_text = clean_text(data_test[0])
+y_test_text = data_test[1]
 
 
 #Применим TfidfVectorizer для преобразования текстов в векторы признаков.
@@ -37,25 +46,13 @@ scoring = list()
 for df_m in min_df_list:
     df_ma = 0.01 + df_m
     while df_ma <= 1:
-
         vectorize = TfidfVectorizer(min_df=df_m, max_df=df_ma)
         X_train = vectorize.fit_transform(X_train_text)
-        
         
         #Используем SGDClassifier кол-во ядер все доступные n_jobs=-1
         classifier = SGDClassifier(loss='log', warm_start=True, n_jobs=-1, max_iter=200, tol=0.001)
         classifier.fit(X_train, y_train)
         
-        #читаем файл csv с имеющимися запросами
-        data_test = pandas.read_csv('storage/data_base_query.csv',  header=None)
-        X_test_text = data_test[0]
-        y_test_text = data_test[1]
-        
-        del data_test
-        
-        X_test_text = X_test_text.map(lambda kkk: kkk.lower())
-        X_test_text = X_test_text.replace('[^а-яА-Яa-zA-Z0-9]', ' ', regex=True)
-        X_test_text = X_test_text.map(lambda x: ' '.join([item.strip() for item in x.split(' ') if item not in stop_w]))
         X_test = vectorize.transform(X_test_text)
         #res = classifier.predict_proba(X_test)
         res = classifier.predict(X_test)
